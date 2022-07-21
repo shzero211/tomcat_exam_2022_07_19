7ccde850763cd29b7a27eef5b96a18cd5ee974f1
SangHun <shzero211@naver.com>
2022-07-21 13:15:02 +0900

    GugudanServlet + HelloServlet 추가

--- src/main/java/com/ll/exam/GugudanServlet.java
+++ src/main/java/com/ll/exam/GugudanServlet.java
@@ -7,13 +7,20 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-@WebServlet("/hello")
-public class HelloServlet extends HttpServlet {
+@WebServlet("/gugudan")
+public class GugudanServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int idx=1;
         req.setCharacterEncoding("UTF-8");
         resp.setContentType("UTF-8");
         resp.setContentType("text/html; charset=utf-8");
-        resp.getWriter().append("<h1>아령하세</h1>");
+        int dan=Integer.parseInt(req.getParameter("dan"));
+        int limit=Integer.parseInt(req.getParameter("limit"));
+            resp.getWriter().append("<h1>%d</h1> <br>".formatted(dan));
+        for(int j=1;j<=limit;j++){
+            resp.getWriter().append("<p>%d x %d = %d</p> <br>".formatted(dan,j,dan*j));
+        }
+
     }
 }
